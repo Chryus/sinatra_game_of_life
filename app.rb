92987c28143e060780cdbe70747feb707c700c8b
Chris <chris.haack@me.com>
2013-12-29 17:02:08 -0500

    add game of life window module

--- app.rb
+++ app.rb
@@ -7,59 +7,63 @@ Dir.glob('./lib/*.rb') do |model|
   require_relative model
 end
 
-class GameOfLifeWindow < Gosu::Window
 
-	def initialize(height=1800, width=1600)
-		@height = height
-		@width = width
-		super height, width, false
-		self.caption = "Gosu Tutorial Game"
+module GameOfLifeWindow < Gosu::Window
+	class App < Sinatra::Application
 
-		#color
-		@background_color = Gosu::Color.new(0xffdedede)
-		@alive_color = Gosu::Color.new(0xff00ffff)
-		@dead_color = Gosu::Color.new(0xffff00ff)
+		def initialize(height=1800, width=1600)
+			@height = height
+			@width = width
+			super height, width, false
+			self.caption = "Gosu Tutorial Game"
 
-		#game itself
-		@cols = width/10
-		@rows = height/10
+			#color
+			@background_color = Gosu::Color.new(0xffdedede)
+			@alive_color = Gosu::Color.new(0xff00ffff)
+			@dead_color = Gosu::Color.new(0xffff00ff)
 
-		@col_width = width/@cols #width div by number of cols
-		@row_height = height/@rows #height div by number of rows
+			#game itself
+			@cols = width/10
+			@rows = height/10
 
-		@world = World.new(@cols, @rows)
-		@game = Game.new(@world)
-		@game.world.randomly_populate
-	end
+			@col_width = width/@cols #width div by number of cols
+			@row_height = height/@rows #height div by number of rows
 
-	def update
-		@game.tick!
-	end
-
-	def draw
-		draw_quad(0, 0, @background_color, #top left corner of scree
-							width, 0, @background_color, #top right corner 
-							width, height, @background_color, #bottom right
-							0, height, @background_color) #bottom left
-
-		@game.world.cells.each do |cell|
-			if cell.alive?
-				draw_quad(cell.x * @col_width, cell.y * @row_height, @alive_color,
-									cell.x * @col_width + @col_width, cell.y * @row_height, @alive_color,
-									cell.x * @col_width + @col_width, cell.y * @row_height + @row_height, @alive_color,
-									cell.x * @col_width, cell.y * @row_height + @row_height, @alive_color)
-			else
-				draw_quad(cell.x * @col_width, cell.y * @row_height, @dead_color,
-									cell.x * @col_width + @col_width, cell.y * @row_height, @dead_color,
-									cell.x * @col_width + @col_width, cell.y * @row_height + @row_height, @dead_color,
-									cell.x * @col_width, cell.y * @row_height + @row_height, @dead_color)
-			end
+			@world = World.new(@cols, @rows)
+			@game = Game.new(@world)
+			@game.world.randomly_populate
 		end
 
-	end
+		def update
+			@game.tick!
+		end
 
-	def needs_cursor?; true; end
+		def draw
+			draw_quad(0, 0, @background_color, #top left corner of scree
+								width, 0, @background_color, #top right corner 
+								width, height, @background_color, #bottom right
+								0, height, @background_color) #bottom left
 
- end
+			@game.world.cells.each do |cell|
+				if cell.alive?
+					draw_quad(cell.x * @col_width, cell.y * @row_height, @alive_color,
+										cell.x * @col_width + @col_width, cell.y * @row_height, @alive_color,
+										cell.x * @col_width + @col_width, cell.y * @row_height + @row_height, @alive_color,
+										cell.x * @col_width, cell.y * @row_height + @row_height, @alive_color)
+				else
+					draw_quad(cell.x * @col_width, cell.y * @row_height, @dead_color,
+										cell.x * @col_width + @col_width, cell.y * @row_height, @dead_color,
+										cell.x * @col_width + @col_width, cell.y * @row_height + @row_height, @dead_color,
+										cell.x * @col_width, cell.y * @row_height + @row_height, @dead_color)
+				end
+			end
+
+		end
+
+		def needs_cursor?; true; end
+
+ 	end
+end
+
 
 GameOfLifeWindow.new.show
